35dc41e72c8edab0244d501838d303b0632752ed
Majid Nachan <mnachan@ap.corp.jwt.com>
2022-07-20 12:15:54 +0530

    updated code

--- execute/index.php
+++ execute/index.php
@@ -1,25 +1,7 @@
 <?php
+$json = file_get_contents('php://input');
 $myfile = fopen("./files/execute.txt", "w") or die("Unable to open file!");
-$txt = json_encode($_POST);
-fwrite($myfile, $txt);
+fwrite($myfile, $json);
 fclose($myfile);
-echo '{"success": true}'
-/*$ch = curl_init();
-  $skipper = "luxury assault recreational vehicle";
-  $fields = array( 'penguins'=>$skipper, 'bestpony'=>'rainbowdash');
-  $postvars = '';
-  foreach($fields as $key=>$value) {
-    $postvars .= $key . "=" . $value . "&";
-  }
-  $url = "http://www.google.com";
-  curl_setopt($ch,CURLOPT_URL,$url);
-  curl_setopt($ch,CURLOPT_POST, 1);                //0 for a get request
-  curl_setopt($ch,CURLOPT_POSTFIELDS,$postvars);
-  curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
-  curl_setopt($ch,CURLOPT_TIMEOUT, 20);
-  $response = curl_exec($ch);
-  print "curl response is:" . $response;
-  curl_close ($ch);
-  */
+echo '{"success": true}';
 ?>
